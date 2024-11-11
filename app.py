b2c69e19d9fb60d88a341fb80147f220f571d41f
Hans Opoku <88454205+HansOheneba@users.noreply.github.com>
2024-11-11 15:34:26 +0000

    procfile added

--- app.py
+++ app.py
@@ -9,13 +9,12 @@ from datetime import datetime
 # Load environment variables
 load_dotenv()
 
-# Initialize Flask app
+
 app = Flask(__name__)
 
-# Configure the database URI
+
 app.config['SQLALCHEMY_DATABASE_URI'] = f"mysql://{os.getenv('user')}:{os.getenv('password')}@{os.getenv('host')}/{os.getenv('database')}"
 
-# Initialize SQLAlchemy with Flask app
 db = SQLAlchemy(app)
 
 class Todo(db.Model):
@@ -35,6 +34,31 @@ def index():
     else:
         tasks = Todo.query.order_by(Todo.date_created).all()
         return render_template("index.html", tasks=tasks)
+
+@app.route('/delete/<id>')
+def delete(id):
+    task_to_delete = Todo.query.get_or_404(id)
+    
+    try:
+        db.session.delete(task_to_delete)
+        db.session.commit()
+        return redirect('/')
+    except:
+        return 'Error Deleting Task'
+    
+@app.route('/update/<id>', methods=['GET', 'POST'])
+def update(id):
+    task = Todo.query.get_or_404(id)
+    if request.method == 'POST':
+        task.content = request.form['content']
+        
+        try:
+            db.session.commit()
+            return redirect('/')
+        except:
+            return 'Error Updating Task'
+    else:
+        return render_template('update.html', task=task)
 
 if __name__ == "__main__":
     app.run(debug=True)
